b8a56c27943ed68399469a67b60649d7b4093e23
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-05-18 16:34:54 +0600

    Update Cart.cs

--- Cart.cs
+++ Cart.cs
@@ -2,7 +2,7 @@ namespace UniversityManagementSystem.Models
 {
     public class Student
     {
-        public int Id { get; set; }
+        
         public string Name { get; set; }
         public string Email { get; set; }
     }
